70f0a4137523bcfbe670ed6400da24cc6b2b38bc
William Tsoi <william@williamtsoi.net>
2015-04-10 15:51:08 +1000

    initial migration

---
+++ wp-content/w3tc-config/master-admin.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+	'version' => '0.9.4',
+	'browsercache.configuration_sealed' => false,
+	'cdn.configuration_sealed' => false,
+	'common.install' => 1407802720,
+	'common.visible_by_master_only' => true,
+	'dbcache.configuration_sealed' => false,
+	'minify.configuration_sealed' => false,
+	'objectcache.configuration_sealed' => false,
+	'pgcache.configuration_sealed' => false,
+	'previewmode.enabled' => false,
+	'varnish.configuration_sealed' => false,
+	'fragmentcache.configuration_sealed' => false,
+	'newrelic.configuration_sealed' => false,
+	'extensions.configuration_sealed' => array(
+	),
+	'notes.minify_error' => true,
+	'minify.error.last' => 'Unable to cache remote file: "http://fonts.googleapis.com/css?family=Source+Sans+Pro%3A300%2C400%2C700%2C300italic%2C400italic%2C700italic%7CBitter%3A400%2C700&subset=latin%2Clatin-ext"',
+	'minify.error.notification' => 'admin',
+	'minify.error.notification.last' => 0,
+	'minify.error.file' => '000000/c7cda/default.include.7652b0.js',
+	'track.maxcdn_signup' => 0,
+	'track.maxcdn_authorize' => 0,
+	'track.maxcdn_validation' => 0,
+	'notes.maxcdn_whitelist_ip' => true,
+	'notes.remove_w3tc' => false,
+	'notes.hide_extensions' => array(
+	),
+	'evaluation.reminder' => 1411709214,
+);
